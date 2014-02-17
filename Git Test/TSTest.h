bcde2dcb81c4ab8e9d869ac777cc7634f6fd25b8
= <=>
2014-02-17 12:25:11 +0000

    Added a test class

---
+++ Git Test/TSTest.h
@@ -0,0 +1,13 @@
+//
+//  TSTest.h
+//  Git Test
+//
+//  Created by Theo Styles on 17/02/2014.
+//  Copyright (c) 2014 Theo Styles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSTest : NSObject
+
+@end
---
+++ Git Test/TSTest.m
@@ -0,0 +1,13 @@
+//
+//  TSTest.m
+//  Git Test
+//
+//  Created by Theo Styles on 17/02/2014.
+//  Copyright (c) 2014 Theo Styles. All rights reserved.
+//
+
+#import "TSTest.h"
+
+@implementation TSTest
+
+@end
